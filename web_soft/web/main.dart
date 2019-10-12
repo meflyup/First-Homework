cd13fa4e8fb18606d027c3aebdf195e223a5ee16
MuRongxin <unity_l@qq.com>
2019-10-12 20:11:20 +0800

    '更改SDK版本为2.4.0'

--- web_soft/web/main.dart
+++ web_soft/web/main.dart
@@ -2,14 +2,15 @@ import 'dart:html';
 import 'dart:math';
 
 void main() {
-  querySelector('#output').text = 'Hello,你准备好了吗？';  
+  querySelector('#output').text = 'Hello,你准备好了吗';
   querySelector('#button').onClick.listen(OnClickButton);
 }
+
 
 void OnClickButton(MouseEvent event){
    int maxVale=int.parse((querySelector('#EnterValue')  as InputElement).value);  
    int random=Random().nextInt(maxVale);
    if(random>0){
-      querySelector('#output').text = 'Hello No.'+random.toString()+' 抽到你了！';
-   }
-}
+      querySelector('#output').text = 'Hello '+random.toString()+' 请你作答';
+    }
+  }
